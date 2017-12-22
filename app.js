43448379c1c0605a730e30b2a1723efb212aa468
Liang Zhang <zhangliangnuaa@gmail.com>
2017-12-22 08:52:21 +0000

    Add register authenticatiojn

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
+    passport    = require("passport"),
+    LocalStrategy = require("passport-local"),
     Campground  = require("./models/campground"),
     Comment     = require("./models/comment"),
+    User        = require("./models/user"),
     seedDB     = require("./seeds");
     
 seedDB();
@@ -13,6 +16,18 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 mongoose.Promise = global.Promise;
 
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Liang Zhang",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // Campground.create(
 //     {
@@ -116,6 +131,28 @@ app.post("/campgrounds/:id/comments", function (req, res) {
         });
        }
    });
+});
+
+//  ===========
+// AUTH ROUTES
+//  ===========
+
+// show register form
+app.get("/register", function(req, res){
+   res.render("register"); 
+});
+//handle sign up logic
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+           res.redirect("/campgrounds"); 
+        });
+    });
 });
 
 app.get("/*", function(req, res) {
